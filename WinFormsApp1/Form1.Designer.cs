9019541a026334f202c21d93ea6a84074fa7f77f
Nazar <biokapt@gmail.com>
2024-12-13 21:53:07 +0200

    UPD: add buttons and changed Form1 + Form1.Designer.

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonCircle = new Button();
+            buttonSquare = new Button();
+            buttonRhomb = new Button();
+            buttonClear = new Button();
+            SuspendLayout();
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(25, 8);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(86, 32);
+            this.buttonCircle.TabIndex = 0;
+            this.buttonCircle.Text = "Circle";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.ButtonCircle_Click);
+            // 
+            // buttonSquare
+            // 
+            this.buttonSquare.Location = new System.Drawing.Point(120, 8);
+            this.buttonSquare.Name = "buttonSquare";
+            this.buttonSquare.Size = new System.Drawing.Size(86, 32);
+            this.buttonSquare.TabIndex = 1;
+            this.buttonSquare.Text = "Square";
+            this.buttonSquare.UseVisualStyleBackColor = true;
+            this.buttonSquare.Click += new System.EventHandler(this.ButtonSquare_Click);
+            // 
+            // buttonRhomb
+            // 
+            this.buttonRhomb.Location = new System.Drawing.Point(215, 8);
+            this.buttonRhomb.Name = "buttonRhomb";
+            this.buttonRhomb.Size = new System.Drawing.Size(86, 32);
+            this.buttonRhomb.TabIndex = 2;
+            this.buttonRhomb.Text = "Rhomb";
+            this.buttonRhomb.UseVisualStyleBackColor = true;
+            this.buttonRhomb.Click += new System.EventHandler(this.ButtonRhomb_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(310, 8);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(86, 32);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.ButtonClear_Click);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(this.buttonCircle);
+            Controls.Add(this.buttonSquare);
+            Controls.Add(this.buttonRhomb);
+            Controls.Add(this.buttonClear);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button buttonCircle;
+        private Button buttonSquare;
+        private Button buttonRhomb;
+        private Button buttonClear;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,53 @@
+using System;
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private Square square;
+        private Rhomb rhomb;
+        private Circle circle;
+
         public Form1()
         {
+
             InitializeComponent();
+            g = this.CreateGraphics();
+            
+        }
+        private void ButtonCircle_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            circle = new Circle(100, 225, 50);
+            DoubleBuffered = true;
+            circle.MoveRight(g);
+
+        }
+
+        private void ButtonSquare_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            square = new Square(400, 225, 100);
+            DoubleBuffered = true;
+            square.MoveRight(g);
+        }
+
+        private void ButtonRhomb_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            rhomb = new Rhomb(400, 225, 120, 80);
+            DoubleBuffered = true;
+            rhomb.MoveRight(g);
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+        
         }
     }
-}
+
+
